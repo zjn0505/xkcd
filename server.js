a8cbcb8568cddf301b73f303ac92a091d9e2283e
Jienan Zhang <zjn0505@hotmail.com>
2018-03-15 07:00:14 +0000

    Add xkcd keyword query

--- server.js
+++ server.js
@@ -1,18 +1,48 @@
-var FCM = require('fcm-push');
 const fcmServerKey = process.env.XKCD_FCM_KEY;
-var fcm = new FCM(fcmServerKey);
 const serverChanKey = process.env.SERVER_CHAN_KEY;
+const xkcdUrl = 'https://xkcd.com/info.0.json';
 var serverChanUrl = 'https://sc.ftqq.com/'+serverChanKey+'.send';
 
-var request = require('request');
-var schedule = require('node-schedule');
-const xkcdUrl = 'https://xkcd.com/info.0.json';
+var FCM = require('fcm-push'),
+	fcm = new FCM(fcmServerKey),
+	request = require('request'),
+	schedule = require('node-schedule'),
+	mongoose = require('mongoose'),
+	express = require('express'),
+	bodyParser = require('body-parser'),
+	app = express(),
+	port = 3003;
 
-var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/xkcd_db');
-
 var latestIndex = 1966;
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.listen(port);
+app.get('/xkcd-suggest', function(req, res) {
+	var keyword = req.query.q;
+	if (!keyword) {
+		res.sendStatus(500);
+		return;
+	}
+	var id = !isNaN(keyword) ? keyword : 0;
+	Xkcd.find(
+	
+	{$or: [
+		{ $text: {$search: keyword}},
+		{ "num" : id}
+	]},
+	{score: { $meta: "textScore" } }).limit(20).sort({score: { $meta: "textScore" } , "num": -1})
+	.exec(
+	function(err, docs) {
+		if(err) {
+			console.error(err);
+		}
+		if(docs) {
+			res.json(docs);
+		}
+	});
+});
 
 var j = schedule.scheduleJob('*/15 * * * *', function() {
         console.log("send request, current index is " + latestIndex);
@@ -50,9 +80,9 @@ var sendNotification = function(xkcd) {
 	};
 	fcm.send(message, function(err, response){
 	    if (err) {
-	        console.log("Something has gone wrong! " + err);
+	        console.log("FCM Something has gone wrong! " + err);
 	    } else {
-	        console.log("Successfully sent with response: ", response);
+	        console.log("FCM Successfully sent with response: ", response);
 	    }
 	});
 }
@@ -76,46 +106,55 @@ function initMongo(latestIndex) {
 
 function queryXkcd(id) {
 	return new Promise(function(resolve, reject) {
-		var xkcdUrl = 'https://xkcd.com/' + id + '/info.0.json';
-		request(xkcdUrl, function(error, response, body) {
-			if (error) {
-				console.error("Query xkcd failed " + id + " " + error);
-				resolve("");
-			}
-			var comics;
-			try {
-				comics = JSON.parse(body);
-			} catch (_err) {
-				console.error("Parse xkcd " + id + " error " + _err);
-				resolve("");
-				return;
-			}
-	        	Xkcd.findOne({'num' : comics.num}, function(err, _xkcd) {
-		            	if (err) {
-		            		reject(Error("Query xkcd failed " + err));
-		            	}
-		            	if (_xkcd == null) {
-		            		var new_xkcd = new Xkcd({
-		            			num: comics.num,
-		            			raw: body,
-	       		    			alt: comics.alt,
-		            			title: comics.title,
-		            			img: comics.img
-		            		});
-		            		new_xkcd.save(function(err, xkcdSaved) {
-		            			if (err) {
-		            				reject(Error("Save xkcd failed" + err));
-		            			}
-		            			if (xkcdSaved) {
-		            				resolve("");
-		            			}
-		            		});
-		            	}
-	        	})              
+		var sendXkcdQuery = function(id) {
+			var xkcdUrl = 'https://xkcd.com/' + id + '/info.0.json';
+			request(xkcdUrl, function(error, response, body) {
+				if (error) {
+					console.error("Query xkcd failed " + id + " " + error);
+					resolve("");
+				}
+				var comics;
+				try {
+					comics = JSON.parse(body);
+				} catch (_err) {
+					console.error("Parse xkcd " + id + " error " + _err);
+					resolve("");
+					return;
+				}
+	            Xkcd.findOne({'num' : comics.num}, function(err, _xkcd) {
+	            	if (err) {
+	            		reject(Error("Query xkcd failed"));
+	            	}
+	            	if (_xkcd == null) {
+	            		var new_xkcd = new Xkcd({
+	            			num: comics.num,
+	            			raw: body,
+	            			alt: comics.alt,
+	            			title: comics.title,
+	            			img: comics.img
+	            		});
+	            		new_xkcd.save(function(err, xkcdSaved) {
+	            			if (err) {
+	            				reject(Error("Save xkcd failed" + err));
+	            			}
+	            			if (xkcdSaved) {
+	            				resolve("");
+	            			}
+	            		});
+	            	}
+	            });            
 	        });
+		}
+		Xkcd.findOne({'num' : id}, function(err, _xkcd) {
+        	if (err || _xkcd == null || (latestIndex - id) < 10) {
+        		sendXkcdQuery(id);
+        		return;
+        	} else {
+        		resolve("");
+        	}
+        });       
 	});
 }
-
 
 var Schema = mongoose.Schema;
 
@@ -124,9 +163,6 @@ var XkcdSchema = new Schema({
         type: Number,
         unique: true,
         min: 1
-    },
-    raw: {
-        type: String
     },
     alt: {
         type: String
@@ -138,7 +174,7 @@ var XkcdSchema = new Schema({
         type: String
     }
 });
-
+XkcdSchema.index({title:'text', alt:'text'}, {name: 'TextIndex', weights: {title: 20, alt: 4}});
 Xkcd = mongoose.model('xkcd', XkcdSchema);
 
 initMongo(latestIndex);
