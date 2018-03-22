4a176f6a59681c19e78f6ada70c25626c4fa55ac
Jienan Zhang <zjn0505@hotmail.com>
2018-03-23 07:44:29 +0800

    Add image size query

--- server.js
+++ server.js
@@ -10,8 +10,10 @@ var FCM = require('fcm-push'),
 	mongoose = require('mongoose'),
 	express = require('express'),
 	bodyParser = require('body-parser'),
+	sizeOf = require('image-size'),
 	app = express(),
 	port = 3003;
+
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/xkcd_db');
@@ -79,53 +81,53 @@ app.get('/xkcd-list', function(req, res) {
 });
 
 var j = schedule.scheduleJob('*/15 * * * *', function() {
-        console.log("send request, current index is " + latestIndex);
-        request(xkcdUrl, function(error, response, body) {
-                var comics = JSON.parse(body)
-                if (comics.num > latestIndex) {
-                	latestIndex = comics.num;
-			queryXkcd(latestIndex);
-                	sendNotification(comics);
-                	console.log("new comics detected, id: " + comics.num);
-			request.post({
-				url: serverChanUrl,
-				form: {text: 'Xkcd-' + comics.num + ' is on the way',
-				       desp: '```json\n'+JSON.stringify(comics, null, 4)+ '\n```\n[![comics]('+comics.img+')]('+comics.img+')'}},
-				function(error, response, body){
-				if (error) { console.log(error); }
-				if (body) { console.log("serverChan " + body); }
-			});
-                }            
-        });
+	console.log("send request, current index is " + latestIndex);
+	request(xkcdUrl, function(error, response, body) {
+			var comics = JSON.parse(body)
+			if (comics.num > latestIndex) {
+				latestIndex = comics.num;
+		queryXkcd(latestIndex);
+				sendNotification(comics);
+				console.log("new comics detected, id: " + comics.num);
+		request.post({
+			url: serverChanUrl,
+			form: {text: 'Xkcd-' + comics.num + ' is on the way',
+				   desp: '```json\n'+JSON.stringify(comics, null, 4)+ '\n```\n[![comics]('+comics.img+')]('+comics.img+')'}},
+			function(error, response, body){
+			if (error) { console.log(error); }
+			if (body) { console.log("serverChan " + body); }
+		});
+			}            
+	});
 });
 
 
 var sendNotification = function(xkcd) {
 	var message = {
-	    to: '/topics/new_comics',
-	    collapse_key: 'new_comics',
-	    data: {
-	        xkcd: xkcd
-	    },
-	    android: {
+		to: '/topics/new_comics',
+		collapse_key: 'new_comics',
+		data: {
+			xkcd: xkcd
+		},
+		android: {
 		ttl: "172800s"
-	    },
-            time_to_live: 172800
+		},
+			time_to_live: 172800
 	};
 	fcm.send(message, function(err, response){
-	    if (err) {
-	        console.log("FCM Something has gone wrong! " + err);
-	    } else {
-	        console.log("FCM Successfully sent with response: ", response);
-	    }
+		if (err) {
+			console.log("FCM Something has gone wrong! " + err);
+		} else {
+			console.log("FCM Successfully sent with response: ", response);
+		}
 	});
 }
 
 function range(start, end, step, offset) {
 	var len = (Math.abs(end - start) + ((offset || 0) * 2)) / (step || 1) + 1;
- 	var direction = start < end ? 1 : -1;
- 	var startingPoint = start - (direction * (offset || 0));
- 	var stepSize = direction * (step || 1);
+	var direction = start < end ? 1 : -1;
+	var startingPoint = start - (direction * (offset || 0));
+	var stepSize = direction * (step || 1);
 	return Array(len).fill(0).map(function(_, index) {
 		return startingPoint + (stepSize * index);
 	});
@@ -155,58 +157,72 @@ function queryXkcd(id) {
 					resolve("");
 					return;
 				}
-	            Xkcd.findOne({'num' : comics.num}, function(err, _xkcd) {
-	            	if (err) {
-	            		reject(Error("Query xkcd failed"));
-	            	}
-	            	if (_xkcd == null) {
-	            		var new_xkcd = new Xkcd({
-	            			num: comics.num,
-	            			raw: body,
-	            			alt: comics.alt,
-	            			title: comics.title,
-	            			img: comics.img
-	            		});
-	            		new_xkcd.save(function(err, xkcdSaved) {
-	            			if (err) {
-	            				reject(Error("Save xkcd failed" + err));
-	            			}
-	            			if (xkcdSaved) {
-	            				resolve("");
-	            			}
-	            		});
-	            	}
-	            });            
-	        });
+				Xkcd.findOne({'num' : comics.num}, function(err, _xkcd) {
+					if (err) {
+						reject(Error("Query xkcd failed"));
+					}
+					request.get({ url: comics.img, encoding: null}, function(err, response, body) {
+						if (err || !body) {
+							reject(Error("Download image failed: " + comics.num));
+						}
+						var dimens = sizeOf(body);
+						var new_xkcd = new Xkcd({
+							num: comics.num,
+							raw: body,
+							alt: comics.alt,
+							title: comics.title,
+							img: comics.img,
+							width: dimens.width,
+							height: dimens.height
+						});
+						new_xkcd.save(function(err, xkcdSaved) {
+							if (err) {
+								reject(Error("Save xkcd failed" + err));
+							}
+							if (xkcdSaved) {
+								resolve("");
+							}
+						});
+					});
+				});
+			});
 		}
 		Xkcd.findOne({'num' : id}, function(err, _xkcd) {
-        	if (err || _xkcd == null || (latestIndex - id) < 10) {
-        		sendXkcdQuery(id);
-        		return;
-        	} else {
-        		resolve("");
-        	}
-        });       
+			if (err || _xkcd == null || (latestIndex - id) < 10 || _xkcd.width == 0) {
+				sendXkcdQuery(id);
+				return;
+			} else {
+				resolve("");
+			}
+		});   
 	});
 }
 
 var Schema = mongoose.Schema;
 
 var XkcdSchema = new Schema({
-    num: {
-        type: Number,
-        unique: true,
-        min: 1
-    },
-    alt: {
-        type: String
-    },
-    title: {
-        type: String
-    },
-    img: {
-        type: String
-    }
+	num: {
+		type: Number,
+		unique: true,
+		min: 1
+	},
+	alt: {
+		type: String
+	},
+	title: {
+		type: String
+	},
+	img: {
+		type: String
+	},
+	width: {
+		type: Number,
+		default: 0
+	},
+	height: {
+		type: Number,
+		default: 0
+	}
 });
 XkcdSchema.index({title:'text', alt:'text'}, {name: 'TextIndex', weights: {title: 20, alt: 4}});
 Xkcd = mongoose.model('xkcd', XkcdSchema);
