4995b824c92c74946eb9993ce883e6a9c4da5ccd
Jienan Zhang <zjn0505@hotmail.com>
2018-07-13 23:59:15 +0800

    Update latest xkcd

--- api/controllers/xkcdController.js
+++ api/controllers/xkcdController.js
@@ -6,7 +6,7 @@ const SERVER_FAILURE_CODE = 400;
 
 var mongoose = require('mongoose'),
 	Xkcd = mongoose.model('xkcd');
-var latestIndex = 2018;
+var latestIndex = 2019;
 exports.latestIndex = latestIndex;
 exports.xkcd_suggest = function (req, res) {
 	var keyword = req.query.q;
