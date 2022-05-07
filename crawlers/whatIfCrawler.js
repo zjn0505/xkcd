67bc89d28f8ee1ec76b67ee54a8c7756efe33162
Jienan Zhang <zjn0505@hotmail.com>
2022-05-07 10:23:54 +0800

    Update what if crawler

--- crawlers/whatIfCrawler.js
+++ crawlers/whatIfCrawler.js
@@ -10,65 +10,76 @@ var WhatIf;
 
 latestWhatIfIndex = routes.latestIndex;
 
-exports.register = function(whatIf) {
+exports.register = function (whatIf) {
 	WhatIf = whatIf;
 }
 
-exports.getLatest = function() {
-	rp(whatIfUrl).then(parseLatest).then(function(article) {
+exports.getLatest = function () {
+	rp(whatIfUrl).then(parseLatest).then(function (article) {
 		latestWhatIfIndex = article.num;
 		routes.setLatest(latestWhatIfIndex);
 		console.log("init what-if and the latest is " + latestWhatIfIndex);
-	}).then(loadDb).catch(function(err) {
+	}).then(loadDb).catch(function (err) {
 		console.error("init what-if Mongo failed " + err);
 	});
 }
 
-exports.regularCheck = function() {
+exports.regularCheck = function () {
 	console.log("what-if regular check, current index is " + latestWhatIfIndex);
-	rp(whatIfUrl).then(parseLatest).then(function(article) {
-		if (article.num > latestWhatIfIndex) {
-			latestWhatIfIndex = article.num;
-			routes.setLatest(latestWhatIfIndex);
-			notifier.newWhatIfForFCM(article);
-			return(article);
-		} else {
-			return Promise.reject("");
-		}      
-	}).then(function(article) { return notifier.newWhatIfForFtqq(article); })
-	.catch(function(err) {
-		console.log(err);
-	})
+	rp(whatIfUrl).then(parseLatest).then(function (article) {
+			if (article.num > latestWhatIfIndex) {
+				latestWhatIfIndex = article.num;
+				routes.setLatest(latestWhatIfIndex);
+				notifier.newWhatIfForFCM(article);
+				return (article);
+			} else {
+				return Promise.reject("");
+			}
+		}).then(function (article) {
+			return notifier.newWhatIfForFtqq(article);
+		})
+		.catch(function (err) {
+			console.log(err);
+		})
 }
 
 function parseLatest(html) {
 	var article = {};
-	var parsed = cheerio.load(html)("article a:first-child");
-	article.num = parseInt(parsed.attr("href").split("/")[3]);
-	article.title = parsed.children().first().html();
+	var $ = cheerio.load(html)
+	article.num = parseInt($("section#entry-wrapper a:first-child").attr("href").split("/")[3]);
+	article.title = $("section#entry-wrapper h2#title a").html();
 	return article;
 }
 
 function loadDb() {
 	return rp(whatIfArchive).then(function (html) {
-		var parsed = cheerio.load(html)("div#archive-wrapper");
-		parsed.contents().map((id, content) => {
-			var num = (id + 1) / 2;
-			if (content.type == "tag") {
-				
-				var title = content.childNodes[3].childNodes[0].childNodes[0].data;
+		var parsed = cheerio.load(html)("div.archive-entry");
+		console.log(`Archive content parsed length ${parsed.length}`)
+		parsed.each((id, content) => {
+			let num = id + 1
+			let parsedEntry = cheerio.load(content)
+			let title = parsedEntry("h2.archive-title a").html()
+			let date = parsedEntry("h3.archive-date").html()
+			let featureImg = "https://what-if.xkcd.com/imgs/a/" + num + "/archive_crop.png";
 
-				WhatIf.findOne({'num' : num}, function(err, whatIf) {
-					if (err || whatIf == null) {
-						whatIf = new WhatIf();
-					}
-					whatIf.num = num;
-					whatIf.title = title;
-					whatIf.date = content.childNodes[5].childNodes[0].data;
-					whatIf.featureImg = "https://what-if.xkcd.com/imgs/a/"+num+"/archive_crop.png";
-					whatIf.save();
-				});
+			if (num == parsed.length) {
+				console.log(`num ${num}, title ${title}, date ${date}, featureImg ${featureImg}`)
 			}
+			
+
+			WhatIf.findOne({
+				'num': num
+			}, function (err, whatIf) {
+				if (err || whatIf == null) {
+					whatIf = new WhatIf();
+				}
+				whatIf.num = num;
+				whatIf.title = title;
+				whatIf.date = date;
+				whatIf.featureImg = featureImg;
+				whatIf.save();
+			});
+
 		});
 	});
 }
